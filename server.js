feab24b4fab19ef9859a734010ab4e21ff05a846
alexr101 <alex_r_101@hotmail.com>
2019-01-09 16:41:32 -0500

    remove ngrok dont need it

--- server.js
+++ server.js
@@ -3,12 +3,6 @@ const path = require('path');
 const app = express();
 const port = process.env.PORT || 8080;
 
-const ngrok = require('ngrok');
-(async function() {
-  const url = await ngrok.connect(port);
-  console.log('ngrok connected on \n', url);
-})();
-
 // Serve any static files
 app.use(express.static(path.join(__dirname, 'client/build')));
 // Handle React routing, return all requests to React app
